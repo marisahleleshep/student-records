6bc26b75c1805fe0e70896c0d206a9a46eb50bfc
marisahleleshep <leleshepmmarisaa@gmail.com>
2023-02-03 07:30:14 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+   var fullName ="marisa leleshep"
+    println(fullName)
+
+    var age = 21
+    println(age)
+    var phoneNumber = 724131503
+    println(phoneNumber)
+    var weightinKg: Byte = 50
+    println(weightinKg)
+    var citizen: Boolean = true
+    println(citizen)
+
+}
